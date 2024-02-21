3937a06c7c8095ed14e7ecd8f6a6eb4d375d4bed
Jeremiah <Teelej18@students.ecu.edu>
2024-02-20 22:55:21 -0500

    Adding login page and sign-out functionality

--- course_project_group_3/course_project_group_3/settings.py
+++ course_project_group_3/course_project_group_3/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'course_project_group_3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Redirects users upon successful login may need to be altered
+LOGIN_REDIRECT_URL = '/'
