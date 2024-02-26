5927acf201db8caec84e348018f93696e769b9f5
Jeremiah <Teelej18@students.ecu.edu>
2024-02-25 19:01:06 -0500

    Adding first name and last name to sign-up page

--- course_project_group_3/accounts/views.py
+++ course_project_group_3/accounts/views.py
@@ -5,9 +5,10 @@ from django.views.generic import CreateView
 from django import forms
 from django.contrib.auth.models import User
 from django.views import generic
+from .forms import CustomUserCreationForm
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 # Create your views here.
